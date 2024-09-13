9a9c645d41f429dd1e91af6baa04a82f74e1f447
m3mo <mehmetozdagg@gmail.com>
2024-09-13 03:10:48 +0200

    first

---
+++ app/src/main/java/com/mozdag/catchthekenny/MainActivity.java
@@ -0,0 +1,118 @@
+package com.mozdag.catchthekenny;
+
+import android.os.Bundle;
+import android.os.CountDownTimer;
+import android.os.Handler;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    TextView timetext;
+    TextView scoreText;
+    int score;
+    ImageView imageView;
+    ImageView imageView1;
+    ImageView imageView2;
+    ImageView imageView3;
+    ImageView imageView4;
+    ImageView imageView5;
+    ImageView imageView6;
+    ImageView imageView7;
+    ImageView imageView8;
+    ImageView imageView9;
+    ImageView imageView10;
+    ImageView imageView11;
+    ImageView[] imageArray;
+    Handler handler;
+    Runnable runnable;
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        //initialize
+
+        timetext = (TextView) findViewById(R.id.timeText);
+        scoreText = (TextView) findViewById(R.id.scoreText);
+
+        imageView = findViewById(R.id.imageView);
+        imageView1 = findViewById(R.id.imageView1);
+        imageView2 = findViewById(R.id.imageView2);
+        imageView3 = findViewById(R.id.imageView3);
+        imageView4 = findViewById(R.id.imageView4);
+        imageView5 = findViewById(R.id.imageView5);
+        imageView6 = findViewById(R.id.imageView6);
+        imageView7 = findViewById(R.id.imageView7);
+        imageView8 = findViewById(R.id.imageView8);
+        imageView9 = findViewById(R.id.imageView9);
+        imageView10 = findViewById(R.id.imageView10);
+        imageView11 = findViewById(R.id.imageView11);
+
+        imageArray = new ImageView[]{imageView,imageView1,imageView2,imageView3,imageView4,imageView5,imageView6,imageView7,imageView8,imageView9,imageView10,imageView11};
+
+        hideImages();
+
+
+
+        score = 0;
+
+        new CountDownTimer(15000,1000){
+            @Override
+            public void onTick(long l) {
+                timetext.setText("Time: " +l/1000);
+            }
+            @Override
+            public void onFinish() {
+            }
+        }.start();
+    }
+
+    public void increaseScore(View view){
+        score++;
+        scoreText.setText("Score: " + score);
+    }
+
+    public void hideImages() {
+
+        handler = new Handler();
+
+        runnable = new Runnable() {
+            @Override
+            public void run() {
+                for (ImageView image : imageArray) {
+                    image.setVisibility(View.INVISIBLE);
+                }
+                Random random = new Random();
+                int i = random.nextInt(12);
+                imageArray[i].setVisibility(View.VISIBLE);
+
+                handler.postDelayed(this,500);
+            }
+
+        };
+
+        handler.post(runnable);
+
+
+
+    }
+}
